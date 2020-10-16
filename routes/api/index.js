c1e9e88204732c5b015dd0deb3c8e78c6b9bb46b
lafry5 <lafry5@yahoo.com>
2020-10-15 19:25:55 -0700

    updated api user-route

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const thoughtRoutes = require('./thought-routes');
 const userRoutes = require('./user-routes');
+// changed comments to thoughts and pizza to user
 
 router.use('/thoughts', thoughtRoutes);
 router.use('/users', userRoutes);
